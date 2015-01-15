2f44f0038b99826d8ec45d579fe732176b0353d8
Sascha Nordquist <sascha.nordquist@andrena.de>
2015-01-15 09:54:21 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,71 @@
+import SonatypeKeys._
+
+
+name := "com.github.httpmock.specs"
+
+version := "0.1"
+
+scalaVersion := "2.11.4"
+
+crossScalaVersions := Seq("2.10.4", "2.11.4")
+
+resolvers ++= Seq(
+  "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository",
+  "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases", // to fix specs2 dependency
+  "Sonatype" at "https://oss.sonatype.org/content/repositories/releases"
+)
+
+libraryDependencies ++= Seq(
+  "org.specs2" %% "specs2" % "2.4.15" % "compile",
+  "com.github.httpmock" % "mock-http-server-junit" % "1.1.6-SNAPSHOT" % "compile",
+  "com.github.httpmock" % "mock-http-server-exec" % "1.1.6-SNAPSHOT" % "compile",
+  "com.github.httpmock" % "mock-http-server-webapp" % "1.1.6-SNAPSHOT" % "compile" artifacts (Artifact("mock-http-server-webapp", "standalone"))
+)
+
+// Scoverage
+scalacOptions in Test ++= Seq("-Yrangepos")
+
+// publishing
+pgpSecretRing := file("local.secring.gpg")
+
+pgpPublicRing := file("local.pubring.gpg")
+
+sonatypeSettings
+
+organization := "com.github.httpmock"
+
+pomExtra := {
+  <url>https://github.com/renesca/renesca</url>
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+      </license>
+    </licenses>
+    <scm>
+      <url>https://github.com/httpmock/mock-http-server-specs</url>
+      <connection>scm:git:git@github.com:httpmock/mock-http-server-specs.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>snordquist</id>
+        <name>Sascha Nordquist</name>
+        <url>https://github.com/snordquist</url>
+      </developer>
+    </developers>
+}
+
+scalacOptions ++= Seq(
+  "-encoding", "UTF-8",
+  "-unchecked",
+  "-deprecation",
+  "-feature",
+  "-Yinline", "-Yinline-warnings",
+  "-language:_"
+)
+
+// support source folders for version specific code
+unmanagedSourceDirectories in Compile <+= (sourceDirectory in Compile, scalaBinaryVersion) {
+  (s, v) => s / ("scala_" + v)
+}
+
---
+++ src/main/scala/com/github/httpmock/specs/HttpMock.scala
@@ -0,0 +1,73 @@
+package com.github.httpmock.specs
+
+import com.github.httpmock.dto.RequestDto
+import com.github.httpmock.rules.{MockService, MockVerifyException, StandaloneMockServer, Stubbing}
+import com.github.httpmock.times.Times
+import com.github.httpmock.{MockServer, PortUtil}
+import com.jayway.awaitility.scala.AwaitilitySupport
+import org.specs2.mutable
+import org.specs2.specification.{After, Fragments, Scope, Step}
+
+trait BeforeAllAfterAll extends mutable.Specification {
+  override def map(fragments: => Fragments) =
+    Step(beforeAll) ^ fragments ^ Step(afterAll)
+
+  protected def beforeAll()
+
+  protected def afterAll()
+}
+
+trait HttpMockServer extends BeforeAllAfterAll with AwaitilitySupport {
+  var mockServer: MockServer = null
+
+  def createMock() = {
+    val mockService = new MockService(baseUri, "/mockserver")
+    mockService.create()
+    mockService
+  }
+
+  def baseUri = mockServer.getBaseUri
+
+  override def beforeAll {
+    val ports = PortUtil.getRandomPorts(3)
+    import com.github.httpmock.ConfigurationBuilder._
+    mockServer = new StandaloneMockServer(config().httpPort(ports.get(0)).stopPort(ports.get(1)).ajpPort(ports.get(2)).build())
+    mockServer.start()
+  }
+
+  override def afterAll {
+    mockServer.stop()
+  }
+}
+
+class HttpMock(val mockService: MockService) extends Scope with After {
+
+  def this(mockServer: HttpMockServer) {
+    this(mockServer.createMock())
+  }
+
+  override def after: Unit = {
+    deleteMock()
+  }
+
+  def deleteMock() {
+    mockService.delete()
+  }
+
+  def when(request: RequestDto) = new Stubbing(mockService, request)
+
+  def verify(request: RequestDto, times: Times) {
+    val numberOfCalls = getNumberOfCalls(request)
+    if (!times.matches(numberOfCalls)) {
+      val expected = times.getFailedDescription
+      throw new MockVerifyException(s"Mock verification failed. Request was called $numberOfCalls times but should have been called $expected")
+    }
+  }
+
+  private def getNumberOfCalls(request: RequestDto) = verifyResponse(request).getTimes
+
+  private def verifyResponse(request: RequestDto) = mockService.verify(request)
+
+  def requestUrl = mockService.getRequestUrl
+
+}
